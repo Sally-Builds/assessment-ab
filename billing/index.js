a1703700177aa108a62427c9dbdc92d2d1f2d5ed
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-05-19 14:09:46 +0100

    customer min requirement

--- billing/index.js
+++ billing/index.js
@@ -5,9 +5,17 @@ const PORT = 3002
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('testing')
+})
+
+
+app.post('/', async (req, res) => {
+    console.log(req.body)
+
+    res.send('done')
 })
 
 
--- customer/index.js
+++ customer/index.js
@@ -1,13 +1,59 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 const PORT = 3000
 const app = express()
 
+app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('testing')
+let customers = [];
+
+app.post('/', async (req, res) => {
+    const {email, first_name, last_name, address, password} = req.body
+
+    const hashedPassword = await bcrypt.hash(password, 10)//This is the hashed password
+
+    customers.push({
+        id: customers.length + 1,
+        email,
+        last_name,
+        first_name,
+        password: hashedPassword,
+        address,
+        fullName:  first_name + " " + last_name,
+        display_name: first_name[0] + last_name[0]
+    })
+
+    res.send({...customers[customers.length -1], password: undefined})
+})
+
+app.post('/login', async(req, res) => {
+    try {
+    const {email, password} = req.body
+
+    const customer = customers.find((el) => el.email == email)
+
+    if(!customer) throw new Error('incorrect email or password')
+
+    if(!(await bcrypt.compare(password, customer.password))) {
+        throw new Error('incorrect email or password')
+    }
+
+    let token =  jwt.sign({id: customer.id}, 'secret', {expiresIn: '30d'})
+
+    res.status(200).json({
+        token,
+        customer: {...customer, password: undefined},
+    })
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({
+            message: error.message,
+        })
+    }
 })
 
 
